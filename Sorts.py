ed86edbb241c29d5ebfc9c82bc00cc9585580283
Veejhay Roy <usroyvj@gmail.com>
2026-02-17 18:05:06 -0500

    Basic sorting

---
+++ Sorts.py
@@ -0,0 +1,20 @@
+arr = [5, 3, 8, 4, 2]
+
+def bubble_sort(arr: list[int]) -> list[int]:
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+                swapped = True
+        
+    return arr
+
+print(arr)
+print(bubble_sort(arr))
+
+
+            
