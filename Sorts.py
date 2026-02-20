cb5cade6442692309c61c8230310be063e05dd01
Veejhay Roy <usroyvj@gmail.com>
2026-02-20 16:41:49 -0500

    More sorts

--- Sorts.py
+++ Sorts.py
@@ -14,9 +14,35 @@ def bubble_sort(arr: list[int]) -> list[int]:
     return arr
 
 def insertion_sort(arr: list[int]) -> list[int]:
+    pass
 
-print(arr)
-print(bubble_sort(arr))
+#print(arr)
+#print(bubble_sort(arr))
 
+def insertion_sort(arr: list[int]) -> list[int]:
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j-1] > arr[j]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
 
-            
+    return arr
+
+def selection_sort(arr: list[int]) -> list[int]:
+    for i in range(len(arr)):
+        min = arr[i]
+        index = i
+        for j in range(i, len(arr)):
+            if arr[j] < min:
+                min = arr[j]
+                index = j
+        arr[i], arr[index] = arr[index], arr[i]
+
+    return arr
+
+arr = [5, 4, 3]
+
+#print(insertion_sort(arr))
+print(selection_sort(arr))
+
