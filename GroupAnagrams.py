bb01446a67791109d186e75b3822af3fdb166142
Veejhay Roy <usroyvj@gmail.com>
2026-02-21 14:02:26 -0500

    RTE

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -5,20 +5,21 @@ def groupAnagrams(strs: list[str]) -> list[list[str]]:
     x = [] 
     seen = []
     for i in range(len(strs)):
-        if seen.__contains__(strs[i]) == True:
+        if seen.__contains__(i) == True:
             continue
         x.append([])
         for j in range(i, len(strs)):
-            if i == j and seen.__contains__(strs[j]) == False:
+            if i == j and seen.__contains__(j) == False:
                 x[-1].append(strs[i])
-                seen.append(strs[i])
+                seen.append(i)
 
-            elif Counter(s) == Counter(t) and seen.__contains__(strs[j]) == False:
+            elif Counter(strs[i]) == Counter(strs[j]) and seen.__contains__(j) == False:
                 x[-1].append(strs[j])
-                seen.append(strs[j])
+                seen.append(j)
             
                 
     return x
 
-strs = ["a"]
+
+strs = ["ab", "ba", "ab"]
 print(groupAnagrams(strs))
